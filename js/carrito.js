50c63bf82a8e589fd8944fae4c23b768655112f7
Luis1080931 <luisu0616@gmail.com>
2023-10-11 16:47:52 -0500

    carrito full terminate

--- js/carrito.js
+++ js/carrito.js
@@ -2,28 +2,51 @@ let boxMinus = document.getElementById("boxMinus")
 let boxPlus = document.getElementById("boxPlus")
 let boxCantidad = document.getElementById("boxCantidad")
 let subTotal = document.getElementById("subTotal")
-let cop = document.getElementById("cop")
+let cantidad = document.getElementById("cantidadProducto")
+let numero = document.getElementById("numeroProducto")
+let valorProducto = 4000000
+let txtProducto = document.getElementById("txtProducto")
+
+let auxValorProducto = new Intl.NumberFormat("de-DE").format(valorProducto)
+document.getElementById("cop").innerHTML = auxValorProducto
 
 boxPlus.addEventListener("click", function() {
     let auxCant = boxCantidad.innerHTML
-    let auxCop = cop.innerHTML
 
     if(auxCant < 10){
         auxCant++;
         boxCantidad.innerHTML = auxCant
+        cantidad.innerHTML = auxCant
+        determinarSingularPlural(auxCant)
+        calcularTotal()
     }
-    subTotal.innerHTML = auxCant*auxCop
 })
 
 boxMinus.addEventListener("click", function() {
-    let auxCanti = boxCantidad.innerHTML
-    let auxiCop = cop.innerHTML
+    let auxCant = boxCantidad.innerHTML
     let auxSub = subTotal.innerHTML
 
-    if(auxCanti <= 10 && auxCanti > 0){
-        auxCanti--;
-        boxCantidad.innerHTML = auxCanti
+    if(auxCant <= 10 && auxCant > 0){
+        auxCant--;
+        boxCantidad.innerHTML = auxCant
+        cantidad.innerHTML = auxCant
+        determinarSingularPlural(auxCant)
+        calcularTotal()
     }
-    subTotal.innerHTML = auxSub-auxiCop
 })
+
+function calcularTotal(){
+    let auxCantidad = boxCantidad.innerHTML
+    let subTotal = document.getElementById("subTotal").innerHTML = valorProducto * Number(boxCantidad.innerHTML)
+
+    let auxSubTotal = new Intl.NumberFormat("de-DE").format(subTotal)
+    document.getElementById("subTotal").innerHTML = auxSubTotal
+}
+function determinarSingularPlural(auxCant){
+    if(auxCant == 0 || auxCant > 1){
+        numero.innerText = "productos"
+    }else if(auxCant == 1){
+        numero.innerText = "producto"
+    }
+}
 
