ade4cebd3c7d3403db054a6aea9fc534d75b103f
Luis1080931 <luisu0616@gmail.com>
2023-11-14 21:47:33 -0500

    amazzon-mas-full

--- js/amazon.js
+++ js/amazon.js
@@ -7,6 +7,52 @@ let img5 = document.getElementById("img-5")
 let modal = document.getElementById("box-modal")
 let close = document.getElementById("close")
 
+let imagePrincipal = document.getElementById("imagePrincipal")
+let image1 = document.getElementById("image-1")
+let image2 = document.getElementById("image-2")
+let image3 = document.getElementById("image-3")
+let image4 = document.getElementById("image-4")
+let image5 = document.getElementById("image-5")
+
+function active(imga){
+    var image = document.querySelectorAll(".imagesReducidas")
+    image.forEach(function (imagen) {
+        imagen.classList.remove('imgActive')
+    })
+    imga.classList.add('imgActive');
+}
+
+image1.addEventListener("mouseover", function() {
+
+    imagePrincipal.src = ''
+    imagePrincipal.src = image1.src
+    active(image1)
+})
+image2.addEventListener("mouseover", function() {
+
+    imagePrincipal.src = ''
+    imagePrincipal.src = image2.src
+    active(image2)
+})
+image3.addEventListener("mouseover", function() {
+
+    imagePrincipal.src = ''
+    imagePrincipal.src = image3.src
+    active(image3)
+})
+image4.addEventListener("mouseover", function() {
+
+    imagePrincipal.src = ''
+    imagePrincipal.src = image4.src
+    active(image4)
+})
+image5.addEventListener("mouseover", function() {
+
+    imagePrincipal.src = ''
+    imagePrincipal.src = image5.src
+    active(image5)
+})
+
 window.onload = () => {
     modal.classList.remove("box-modal")
 }
